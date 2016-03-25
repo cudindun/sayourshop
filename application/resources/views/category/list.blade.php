0375c7dc86d8cc3c90278043884b20277404b0cf
fnhaven <cudindun@gmail.com>
2016-03-26 06:52:10 +0700

    fixed layout + add url - add list data

---
+++ application/resources/views/category/list.blade.php
@@ -0,0 +1,57 @@
+		<section class="content-header">
+          <h1>
+            Category
+            <small>List</small>
+          </h1>
+          <ol class="breadcrumb">
+            <li><a href="{{url('/master')}}"><i class="fa fa-home"></i> Home</a></li>
+            <li><a href="#"><i class="fa fa-folder"></i> Category</a></li>
+            <li><a href="{{url('/master/category/list')}}"></i> List</a></li>
+          </ol>
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+          
+          <!-- Default box -->
+          <div class="box box-warning">
+            <div class="box-header with-border">
+
+            </div>
+            <div class="box-body">
+            	<?php // ======================== Data Table ================================ ?>
+              	  <table id="category_table" class="table table-bordered table-hover">
+                    <thead>
+                      <tr>
+                        <th>No</th>
+                        <th>Nama Kategori</th>
+                        <th>Total Product</th>
+                        <th>Slug</th>
+                        <th>Total Subkategori</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <?php $i=1; foreach($data['category'] as $category): ?> 
+                      <tr>
+                        <td><?= $i ?></td>
+                        <td><?= $category->name ?></td>
+                        <td><?= $category->total_product ?></td>
+                        <td><?= $category->slug ?></td>
+                        <td><?= $category->subcategories ?></td>
+                      </tr>
+                      <?php $i++; endforeach; ?>
+                    </tbody>
+                  </table>
+                  <?php // ======================== END Data Table ================================ ?>
+            </div><!-- /.box-body -->
+          </div><!-- /.box -->
+
+        </section><!-- /.content -->
+
+@section('script')
+	<script>
+      $(function () {
+        $("#category_table").DataTable();
+      });
+    </script>
+@stop
---
+++ application/resources/views/subcategory/list.blade.php
@@ -0,0 +1,59 @@
+		<section class="content-header">
+          <h1>
+            SubCategory
+            <small>List</small>
+          </h1>
+          <ol class="breadcrumb">
+            <li><a href="{{url('/master')}}"><i class="fa fa-home"></i> Home</a></li>
+            <li><a href="#"><i class="fa fa-file"></i> SubCategory</a></li>
+            <li><a href="{{url('/master/subcategory/list')}}"></i> List</a></li>
+          </ol>
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+          
+          <!-- Default box -->
+          <div class="box box-warning">
+            <div class="box-header with-border">
+
+            </div>
+            <div class="box-body">
+            	<?php // ======================== Data Table ================================ ?>
+              	  <table id="subcategorylist_table" class="table table-bordered table-hover">
+                    <thead>
+                      <tr>
+                        <th>No</th>
+                        <th>Nama Kategori</th>
+                        <th>Nama Subkategori</th>
+                        <th>Slug</th>
+                        <th>Properties</th>
+                        <th>Total Produk</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <?php $i=1; foreach($data['subcategory'] as $subcategory): ?> 
+                      <tr>
+                        <td><?= $i ?></td>
+                        <td><?= $subcategory->category->name ?></td>
+                        <td><?= $subcategory->subname ?></td>
+                        <td><?= $subcategory->slug ?></td>
+                        <td><?= $subcategory->properties ?></td>
+                        <td><?= $subcategory->total_product ?></td>
+                      </tr>
+                      <?php $i++; endforeach; ?>
+                    </tbody>
+                  </table>
+                  <?php // ======================== END Data Table ================================ ?>
+            </div><!-- /.box-body -->
+          </div><!-- /.box -->
+
+        </section><!-- /.content -->
+
+@section('script')
+	<script>
+      $(function () {
+        $("#subcategorylist_table").DataTable();
+      });
+    </script>
+@stop
