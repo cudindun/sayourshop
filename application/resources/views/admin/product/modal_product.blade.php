9fcd4d7123f3243b69f4e172f6973e3fec686ede
adeard <adeardian1994@gmail.com>
2016-04-20 11:09:38 +0700

    fix insert_product

--- application/resources/views/admin/product/modal_product.blade.php
+++ application/resources/views/admin/product/modal_product.blade.php
@@ -59,22 +59,29 @@ th{
           </div>
 
           <div class="tab-pane fade" id="detail-atribute">
-            <table class="table table-bordered table-responsive" style="text-align: center;">
-              <thead>
-                <tr>
-                  @foreach($data['size'] as $size)
-                    <th style="text-align: center;background-color: #3c8dbc;">{{$size->size}}</th>
-                  @endforeach
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  @foreach($data['size'] as $quantity)
-                  <td style="text-align: center;">{{$size->quantity}}</td>
-                  @endforeach
-                </tr>
-              </tbody>
-            </table>
+            <?php $properties = unserialize($data['product']->properties);?>
+
+            <div class="col-sm-12">
+            <input type="hidden" id="product_id" value="{{$data['product']->id}}"></input>
+            <br>
+              @foreach($properties as $key => $value)
+                <table class="table-responsive col-sm-4">
+                  <tr>
+                    <th rowspan="{{count($value)+1}}" style="background-color: #367fa9;color: white;text-align: center;min-width: 10px;margin: 10px;">{{ucwords($key)}}</th>
+                    <td>
+                      @foreach($value as $size => $qty)
+                        <tr>
+                          <input type="hidden" value="{{$qty}}" id="tmp_{{$size}}"></input>
+                          <td style="padding: 10px;">{{strtoupper($size)}}</td>
+                          <td>: <input class="qty"  type="number" value="{{$qty}}" id="{{$size}}" name="{{$key}}" ></input> pcs</td>
+                        </tr>
+                      @endforeach
+                    </td>
+                  </tr>
+                </table>
+              @endforeach
+            </div>
+            <div id="attribute"></div>
           </div>
 
           <div class="tab-pane fade" id="detail-order" style="padding: 10px;">
@@ -117,10 +124,48 @@ th{
 <script type="text/javascript">
 $(document).ready(function()
   {
+    var coba = 'tes';
     $('#detail').modal('show');
 
     $(function () {
         $("#order_table").DataTable();
+    });
+
+    $('.properties').click(function()
+    {
+      var id = this.id;
+      var color = this.name;
+      $.ajax({
+        url: "{!! url('ajax_modal_attr') !!}",
+        data: {
+          id: id,
+          color: color
+        },
+        method:'GET',
       });
+    });
+
+    $('.qty')
+    .on('focus', function(){
+      var val = $(this).val();
+      console.log(coba);
+    })
+    .change(function()
+    {
+      var id = this.id;
+      var qty = $("#"+id).val();
+      var color = this.name;
+      console.log(coba);
+      // $.ajax({
+      //   url: "{!! url('edit_qty') !!}",
+      //   data: {
+      //     product_id:product_id,
+      //     color: color,
+      //     id: id,
+      //     qty: qty
+      //   },
+      //   method:'POST',
+      // });
+    });
 });
 </script>
