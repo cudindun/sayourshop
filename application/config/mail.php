d14d11dffe3d30c520d841eb5bde12235c10177b
fnhaven <cudindun@gmail.com>
2016-04-27 11:34:23 +0700

    fix mail + template account activation

--- application/config/mail.php
+++ application/config/mail.php
@@ -93,7 +93,7 @@ return [
     |
     */
 
-    'password' => 'yzqsyqaugumapohu',
+    'password' => 'ikigipwoclvwoaet',
 
     /*
     |--------------------------------------------------------------------------
--- application/resources/views/email/account_activation.blade.php
+++ application/resources/views/email/account_activation.blade.php
@@ -1,11 +1,40 @@
 <body>
-    <p>CLick the following URL to activated your account : </p><br/>
+	<div class="container">
+		<div class="row">
+			<div style="background-color:#aaa;padding:4px">
+				<div style="background-color:white;padding:15px">
+				    <p>CLick the following URL to activated your account : </p>
 
-    <p>{{ url('/account-activation/'.$id.'&key='.$key) }}</p>
+				    <p>{{ url('/account-activation/'.$id.'&key='.$key) }}</p>
 
-    <p>{{ $data['email'] }}</p>
-    <p>{{ $data['password'] }}</p>
-    <p>{{ $data['first_name'] }}</p>
-    <p>{{ $data['last_name'] }}</p>
+				    <p>Your Secret Information : </p>
+
+				    <table class="table table-hover" style="width:90%;padding:8px">
+					    <tr>
+					    	<th style="width:10%;text-align:left">Email</th>
+					    	<td style="width:40%">{{ $data['email'] }}</td>
+					    </tr>
+					    <tr>
+					    	<th style="width:10%;text-align:left">Password</th>
+					    	<td style="width:40%">{{ $data['password'] }}</td>
+					    </tr><tr>
+					    	<th style="width:10%;text-align:left">First Name</th>
+					    	<td style="width:40%">{{ $data['first_name'] }}</td>
+					    </tr><tr>
+					    	<th style="width:10%;text-align:left">Last Name</th>
+					    	<td style="width:40%">{{ $data['last_name'] }}</td>
+					    </tr>
+					    <tr>
+					    	<th style="width:10%;text-align:left">phone</th>
+					    	<td style="width:40%">{{ $data['phone'] }}</td>
+					    </tr>
+				    </table>
+
+				    <p><font style="color:red">*Your Password are encrypted in our database!</font></p>
+
+			    </div>
+		    </div>
+	    </div>
+    </div>
 
 </body>
