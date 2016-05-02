181ac071632c0beb18475148f13c9cca3bf8d902
fnhaven <cudindun@gmail.com>
2016-05-02 20:20:34 +0700

    fix2

--- application/resources/views/cost_content.blade.php
+++
@@ -1,9 +0,0 @@
-<table class="table">
-	<?php $result = unserialize($data['cost_data'])?>
-	@foreach ($result as $key => $value)
-		<tr>
-			<td>{{$result[$key]->service}} {{$result[$key]->cost[0]->etd}} hari </td>
-			<td class="pull-right">Rp. {{ number_format($result[$key]->cost[0]->value, 0, ",", ".") }}</td>
-		</tr>
-	@endforeach
-</table>
